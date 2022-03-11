bd43f560d25cfda6dee87e341a3c93e25d00abd2
K1ngS <50477395+kingsworm0@users.noreply.github.com>
2022-03-11 03:49:44 +0200

    db config f

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const creds ={
      "password:'',
       "db":"nodelogin,
   }
-const  exec = {
+const  exec = ["
 CREATE DATABASE IF NOT EXISTS `nodelogin` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `nodelogin`;
 
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');}
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');}"]
 
 
 
